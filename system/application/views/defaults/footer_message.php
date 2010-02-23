6e50ae391d86c80870e1f9eaf0e19a7200cc5ba5
Chris Dzombak <chris@chrisdzombak.net>
2010-02-23 17:54:57 -0500

    Licensing and (c) updates.

--- system/application/views/defaults/footer_message.php
+++ system/application/views/defaults/footer_message.php
@@ -1,3 +1,3 @@
 <div class="footer">
-	<p>zombiePaste modified by <a href="http://chris.dzombak.name">Chris Dzombak</a> based on <a href="http://code.google.com/p/stikked/">Stikked</a> by <a href="http://benmcredmond.com">Ben McRedmond</a>.</p>
+	<p>Stuck by <a href="http://chris.dzombak.name">Chris Dzombak</a> based on <a href="http://code.google.com/p/stikked/">Stikked</a> by <a href="http://benmcredmond.com">Ben McRedmond</a>.</p>
 </div>
